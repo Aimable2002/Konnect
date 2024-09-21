430abb9f3b0a3d5e40eaa38bcb20f6b9c93abf61
aimable <aganzerizen@gmail.com>
2024-09-21 16:18:40 +0200

    another commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,11 @@
 import express from 'express'
-import dotenv from 'dotenv'
-import connectDatabase from './database/connectDatabase.js'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import path from 'path'
-
-import authRoute from './Router/authRoute.js'
-import userRoute from './Router/userRoute.js'
-
-
 const app = express()
+const port = 3000
 
-const __dirname = path.resolve();
-dotenv.config()
-
-const PORT = process.env.PORT || 3000
-
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(cors())
-
-app.use('/api/authUser', authRoute);
-app.use('/api/user', userRoute)
-
-
-app.use(express.static(path.join(__dirname, "/Frontend/dist")))
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-connectDatabase()
-// connectCloudinary()
-app.listen(PORT, () => {
-    
-    console.log(`server running on port : ${PORT}`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
